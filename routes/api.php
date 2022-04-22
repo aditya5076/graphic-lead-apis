12181ff13496c18bdc332bc77a5ec757576125c2
aditya <aditya@chicstays.com>
2022-04-22 23:54:48 +0530

    fixed get json error again

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ Route::post('login', 'AuthController@login');
 Route::middleware(['jwt.verify'])->group(function () {
     Route::post('logout', 'AuthController@logout');
     Route::post('imageqr', 'ImageQrController@store')->middleware('json.verify');
-    Route::put('imageqr/{uuid}', 'ImageQrController@update')->middleware('json.verify');
+    Route::put('imageqr/{uuid}', 'ImageQrController@update');
     Route::get('imageqr/{uuid}', 'ImageQrController@show');
     Route::get('imagequeue', 'ImageQrController@index');
 });
